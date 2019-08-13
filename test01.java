39417d300516de0ed9073dc7db97fb397ce2cadf
샤페리우스 <46657969+kdm1993@users.noreply.github.com>
2019-08-13 16:46:33 +0900

    0.04
    - 폭발 이펙트 출력 메소드 구현
    - 충돌 메소드 연산 수정
    - 오른쪽 상단에 스코어 String 출력

--- test01.java
+++ test01.java
@@ -20,38 +20,34 @@ class test02 extends JFrame implements KeyListener, Runnable, MouseListener {
 	
 	int x = 100 , y = 300;
 	int map_x_1 = 0, map_x_2 = 1280;
-	int f_width = 1280, f_height=720;
+	int f_width = 1280, f_height=710;
 	Toolkit tk = Toolkit.getDefaultToolkit();
 	Image ironman = tk.getImage("D://images//ironman_up.png");
 	Image bullet = tk.getImage("D://images//bullet.png");
 	Image kill = tk.getImage("D://images//kill.png");
 	Image map = tk.getImage("D://images//map.jpg");
-	Image map2 = tk.getImage("D://images//map.jpg");
+	Image map2 = tk.getImage("D://images//map2.png");
 	Image buffImage = null;
 	Graphics buffg = null;
 	Thread th;
 	ArrayList bullet_list = new ArrayList();
 	ArrayList kill_list = new ArrayList();
 	ArrayList explo_list = new ArrayList();
-	ArrayList explo_img = new ArrayList();
+	Image explosion;
 	Bullet b;
 	Kill k;
 	Explosion e;
 	int bullet_count = 0, kill_count = 0;
 	boolean left, right, up, down, left_button, left_button_count;
-	boolean bullet_check;
+	boolean bullet_check_x, bullet_check_y;
 	
 	public test02() {
-		for(int i=0; i<20; i++) {
-			explo_img.add(tk.getImage("D://images//.jpg"));
-		}
-		Dimension dim = new Dimension(f_width, f_height);
+		Dimension dim = new Dimension(1280, 720);
 		setPreferredSize(dim);
 		pack();
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		addKeyListener(this);
 		addMouseListener(this);
-		setBackground(Color.white);
 		setTitle("½´ÆÃ°ÔÀÓ");
 		setFocusable(true);
 		setVisible(true);
@@ -60,7 +56,7 @@ class test02 extends JFrame implements KeyListener, Runnable, MouseListener {
 	}
 	
 	public void paint(Graphics g) {
-		buffImage = createImage(f_width, f_height+15);
+		buffImage = createImage(f_width, f_height);
 		buffg = buffImage.getGraphics();
 		update(g);
 	}
@@ -69,9 +65,6 @@ class test02 extends JFrame implements KeyListener, Runnable, MouseListener {
 		Draw_Bullet();
 		Draw_Kill();
 		Draw_Explosion();
-		if(bullet_list.size() != 0) {
-			Bullet_Check();
-		}
 		g.drawImage(buffImage, 0, 0, this);
 	}
 	@Override
@@ -82,6 +75,7 @@ class test02 extends JFrame implements KeyListener, Runnable, MouseListener {
 				KeyProcess();
 				BulletProcess();
 				KillProcess();
+				Bullet_Check();
 				Map_Move();
 				repaint();
 				Thread.sleep(20);
@@ -91,16 +85,19 @@ class test02 extends JFrame implements KeyListener, Runnable, MouseListener {
 		}
 	}
 	public void Draw() {
-		buffg.clearRect(0, 15, f_width, f_height);
+		buffg.clearRect(0, 30, f_width, f_height);
 		buffg.drawImage(map, map_x_1, 0, this);
 		buffg.drawImage(map2, map_x_2, 0, this);
+		buffg.setColor(Color.blue);
+		buffg.setFont(new Font("ÈÞ¸ÕµÕ±ÙÇìµå¶óÀÎ", Font.BOLD, 30));
+		buffg.drawString("SCORE : " + new Score().score, 1000, 58);
 		buffg.drawImage(ironman, x, y, this);
 	}
 	public void KeyProcess() {
-		if(up == true && y>15) {
+		if(up == true && y>30) {
 			y-=10;
 		}
-		if(down == true && y<600) {
+		if(down == true && y<580) {
 			y+=10;
 		}
 		if(left == true && x>200) {
@@ -113,7 +110,7 @@ class test02 extends JFrame implements KeyListener, Runnable, MouseListener {
 	public void BulletProcess() {
 		bullet_count++;
 		if(left_button == true && bullet_count>=7) {
-			b = new Bullet(x+ironman.getWidth(this), y);
+			b = new Bullet(x+ironman.getWidth(null), y);
 			bullet_list.add(b);
 			bullet_count = 0;
 		}
@@ -121,7 +118,7 @@ class test02 extends JFrame implements KeyListener, Runnable, MouseListener {
 	public void KillProcess() {
 		kill_count++;
 		if(kill_count>=20) {
-			k = new Kill(f_width, (int)(Math.random()*(f_height-kill.getHeight(this)+16)));
+			k = new Kill(f_width, (int)(Math.random()*((f_height-kill.getHeight(null)-100)))+30);
 			kill_list.add(k);
 			kill_count = 0;
 		}
@@ -130,27 +127,39 @@ class test02 extends JFrame implements KeyListener, Runnable, MouseListener {
 		try {
 			for(int i=0; i<bullet_list.size(); i++) {
 				for(int v=0; v<kill_list.size(); v++) {
-					if(bullet_list.size() != 0) {
+					if(i!=bullet_list.size()) {
 						b = (Bullet) bullet_list.get(i);
 					}
-					if(kill_list.size() != 0) {
+					if(v!=kill_list.size()) {
 						k = (Kill) kill_list.get(v);
 					}
-					if((b.pos.x >= k.kill_pos.x && b.pos.x <= k.kill_pos.x+kill.getWidth(this))
-						&& (b.pos.y >= k.kill_pos.y && b.pos.y <= k.kill_pos.y+kill.getHeight(this))) {
-						bullet_check = true;
+					for(int x1=b.pos.x; x1<b.pos.x+bullet.getWidth(null); x1++) {
+						for(int x2=k.kill_pos.x; x2<k.kill_pos.x+kill.getWidth(null); x2++) {
+							if(x1 == x2) {
+								bullet_check_x = true;
+							}
+						}
 					}
-					if(bullet_check == true) {
-						//e = new Explosion(k.kill_pos.x, k.kill_pos.y);
-						//explo_list.add(e);
+					for(int y1=b.pos.y; y1<b.pos.y+bullet.getHeight(null); y1++) {
+						for(int y2=k.kill_pos.y; y2<k.kill_pos.y+kill.getHeight(null); y2++) {
+							if(y1 == y2) {
+								bullet_check_y = true;
+							}
+						}
+					}
+					if(bullet_check_x == true && bullet_check_y == true) {
+						e = new Explosion(k.kill_pos.x, k.kill_pos.y);
+						new Score().score_plus(10);
+						explo_list.add(e);
 						bullet_list.remove(i);
 						kill_list.remove(v);
 					}
-					bullet_check = false;
+					bullet_check_x = false;
+					bullet_check_y = false;
 				}
 			}
 		} catch(Exception e) {
-			
+			e.printStackTrace();
 		}
 		
 	}
@@ -187,12 +196,13 @@ class test02 extends JFrame implements KeyListener, Runnable, MouseListener {
 	public void Draw_Explosion()  { //Æø¹ßÀÌÆåÆ® Ãâ·Â¸Þ¼Òµå
 		for(int i=0; i<explo_list.size(); i++) {
 			e = (Explosion) explo_list.get(i);
-			Image explosion = tk.getImage("D://images//explosion.gif");
+			explosion = tk.getImage("D://images//explosion//explosion"+e.effect_num+".png");
 			buffg.drawImage(explosion, e.explo_pos.x, e.explo_pos.y, this);
-			e.move();
-			if(e.explo_pos.x <= 0-explosion.getWidth(this)) {
+			e.next_img();
+			if(e.effect_num >= 19) {
 				explo_list.remove(i);
 			}
+			e.move();
 		}
 	}
 	public void keyPressed(KeyEvent e) {
@@ -277,7 +287,14 @@ class Bullet {
 		pos = new Point(x, y);
 	}
 	public void move() {
-		pos.x += 10;
+		pos.x += 30;
+	}
+}
+class Score {
+	static int score=0;
+	
+	public void score_plus(int x) {
+		score += x;
 	}
 }
 class Kill {
@@ -292,6 +309,7 @@ class Kill {
 }
 class Explosion {
 	Point explo_pos;
+	int effect_num = 0;
 	
 	Explosion(int x, int y) {
 		explo_pos = new Point(x, y);
@@ -299,12 +317,7 @@ class Explosion {
 	public void move() {
 		explo_pos.x -= 5;
 	}
-}
-class Explosion_Img {
-	
-	
-	
-	Explosion_Img() {
-		
+	public void next_img() {
+		effect_num++;
 	}
 }
